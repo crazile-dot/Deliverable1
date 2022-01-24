10517bf01202becdec8d9b34eea8fce2f7bc3281
crazile-dot <ilenia.rocca@live.it>
2022-01-24 20:35:38 +0100

    smell code 11

--- src/main/RetrieveTicketsID.java
+++ src/main/RetrieveTicketsID.java
@@ -25,6 +25,8 @@ import org.json.JSONArray;
 
 public class RetrieveTicketsID {
 	
+	private static String PROJ_NAME = "RAMPART";
+	
 	private static final Logger LOGGER = Logger.getLogger(RetrieveTicketsID.class.getName());
 	
     private static String readAll(Reader rd) throws IOException {
@@ -67,10 +69,50 @@ public class RetrieveTicketsID {
 	   return new SimpleDateFormat(format).parse(string);
     }
 
+    public static List<Integer> makeZeroList(List<Date> ticketList, Calendar cal) {
+		List<Integer> ret = new ArrayList<>();
+		int ticketSize = ticketList.size();
+		// prendo la lista dei ticket e creo una lista con tanti zeri quanti sono i mesi totali dal primo all'ultimo ticket
+	    cal.setTime(ticketList.get(0));
+	    int startYear = cal.get(Calendar.YEAR);
+	    cal.setTime(ticketList.get(ticketSize-1));
+	    int endYear = cal.get(Calendar.YEAR);
+	    int nMonths = ((endYear+1)-startYear)*12;
+	    
+	    for(int l = 0; l < nMonths; l++) {
+	    	ret.add(0);
+	    }
+	    ret.add(startYear);
+	    
+	    return ret;
+    }
+    
+    public static List<Integer> fillZeroList(List<Date> ticketList, int nMonths, int ticketSize, Calendar cal, List<Integer> zeroList) {
+    	//contiamo quanti ticket ci sono relativi ad ogni mese
+        for(int c1 = 0; c1 < ticketSize; c1++) {
+       	 int ticketCounter = 1;
+       	 cal.setTime(ticketList.get(c1));
+       	 int year1 = cal.get(Calendar.YEAR);
+            int month1 = cal.get(Calendar.MONTH);
+            for(int c2 = 1 + c1; c2 < ticketSize; c2++) {
+           	 cal.setTime(ticketList.get(c2));
+           	 int year2 = cal.get(Calendar.YEAR);
+                int month2 = cal.get(Calendar.MONTH);
+           	 if(month1 == month2 && year1 == year2) {
+           		 ticketCounter = ticketCounter + 1;
+           	 }
+           	 else
+           		 break;
+            }
+            //riempiamo la lista con il numero di ticket per ciascun mese
+            int index = ((year1-(nMonths-1))*12)+month1;
+            zeroList.set(index, ticketCounter);
+        }
+        return zeroList;
+    }
 
     public static void main(String[] args) throws IOException, JSONException, ParseException {
 		   
-    	String projName ="RAMPART";
     	Integer j = 0;
     	Integer i = 0;
     	Integer total = 1;
@@ -79,7 +121,7 @@ public class RetrieveTicketsID {
 			 //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
 			 j = i + 1000;
 			 String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
-			+ projName + "%22AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
+			+ PROJ_NAME + "%22AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
 			+ i.toString() + "&maxResults=" + j.toString();
 			
 			 JSONObject json = readJsonFromUrl(url);
@@ -102,39 +144,11 @@ public class RetrieveTicketsID {
 			 int ticketSize = ticketList.size();
 			 System.out.println(ticketList);
 			 
-			 // prendo la lista dei ticket e creo una lista con tanti zeri quanti sono i mesi totali dal primo all'ultimo ticket
-			 List<Integer> ret = new ArrayList<>();
 			 Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"));
-	         cal.setTime(ticketList.get(0));
-	         int startYear = cal.get(Calendar.YEAR);
-	         cal.setTime(ticketList.get(ticketSize-1));
-	         int endYear = cal.get(Calendar.YEAR);
-	         int nMonths = ((endYear+1)-startYear)*12;
+			 List<Integer> ret = makeZeroList(ticketList, cal);
+			 int nMonths = ret.size();
+			 List<Integer> filled = fillZeroList(ticketList, nMonths, ticketSize, cal, ret);
 	         
-	         for(int l = 0; l < nMonths; l++) {
-	        	 ret.add(0);
-	         }
-	         
-	         //contiamo quanti ticket ci sono relativi ad ogni mese
-	         for(int c1 = 0; c1 < ticketSize; c1++) {
-	        	 int ticketCounter = 1;
-	        	 cal.setTime(ticketList.get(c1));
-	        	 int year1 = cal.get(Calendar.YEAR);
-	             int month1 = cal.get(Calendar.MONTH);
-	             for(int c2 = 1 + c1; c2 < ticketSize; c2++) {
-	            	 cal.setTime(ticketList.get(c2));
-	            	 int year2 = cal.get(Calendar.YEAR);
-	                 int month2 = cal.get(Calendar.MONTH);
-	            	 if(month1 == month2 && year1 == year2) {
-	            		 ticketCounter = ticketCounter + 1;
-	            	 }
-	            	 else
-	            		 break;
-	             }
-	             //riempiamo la lista con il numero di ticket per ciascun mese
-	             int index = ((year1-startYear)*12)+month1;
-	             ret.set(index, ticketCounter);
-	         }
 	         //sum è il numero totale di ticket nell'intero periodo osservato
 	         int sum = 0;
 	         for(Integer elem: ret) {
